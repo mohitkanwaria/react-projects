45deaa3e3082cba0e22d2c974ab1767eb8fd0681
mohitkanwaria <mohit.kanwaria.5@gmail.com>
2024-12-01 12:07:04 +0530

    Brand Page

---
+++ Brand Page/src/components/Navigation.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <nav className="container">
+    <div className="logo">
+      <img src="/images/Frame 3.jpg" alt="logo" />
+    </div>
+
+    <ul>
+      <li href="#">Menu</li>
+      <li href="#">Location</li>
+      <li href="#">About</li>
+      <li href="#">Contact</li>
+    </ul>
+
+    <button>Login</button>
+  </nav>
+  )
+}
+
+export default Navigation
